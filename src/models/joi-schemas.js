012ba6233a611f216d054f3abfb4972a2cfffb89
Adam Gibson <adamhenrygibson@gmail.com>
2024-02-24 17:45:11 +0000

    added additional JOI validation for adding teams and adding employees

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -6,3 +6,19 @@ export const UserSpec = {
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
+
+export const UserCredentialsSpec = {
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const TeamSpec = {
+  teamName: Joi.string().required(),
+};
+
+export const EmployeeSpec = {
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().required(),
+  supervisor: Joi.string().required(),
+};
